73a4dd29adacfe025a9f68061747baae55c79579
Alessandro Tegner <alessandro.tegner@gmail.com>
2018-01-26 10:19:18 -0200

    Improve string normalizer

---
+++ go/gophercises/phone-number-normalizer/src/phone/normalizer.go
@@ -0,0 +1,17 @@
+package phone
+
+import (
+	"bytes"
+)
+
+func Normalize(phone string) string {
+	var normalized bytes.Buffer
+
+	for _, char := range phone {
+		if char >= '0' && char <= '9' {
+			normalized.WriteRune(char)
+		}
+	}
+
+	return normalized.String()
+}
